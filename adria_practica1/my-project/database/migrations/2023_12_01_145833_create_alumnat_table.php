5be6bb5f6dd7fdd90ce723eceb193fa1b9fe5ad3
Adria <adriagp90@gmail.com>
2023-12-01 16:52:56 +0100

    afegit migrations

---
+++ adria_practica1/my-project/database/migrations/2023_12_01_145833_create_alumnat_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+/* Migration que crea la taula 'professorat' dins de la nostra base de dades amb els atributs indicats */
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('professorat', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->char('name', 100);
+            $table->string('surname');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('professorat');
+    }
+};
