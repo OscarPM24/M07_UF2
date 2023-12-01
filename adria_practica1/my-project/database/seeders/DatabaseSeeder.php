26ca836c80c3f9a25c321d47749a873145c1a812
Adria <adriagp90@gmail.com>
2023-12-01 16:53:12 +0100

    afegit seeders

--- adria_practica1/my-project/database/seeders/DatabaseSeeder.php
+++ adria_practica1/my-project/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Centre;
+use App\Models\Alumnat;
+use App\Models\Professorat;
+// per executar el seeder fem php artisan db:seed
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        /*
+        | Crea 5 registres per cada taula utilitzant les seves factories
+            NOTA: No sobreescriu, es sumatori.
+        */
+        Centre::factory(5)->create();
+        Alumnat::factory(5)->create();
+        Professorat::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
